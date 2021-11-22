ba8206ace4f3e54f37220fcf8049915ec39a2bfa
GONZALES-LAPTOP\rvg_2 <rvg_23@yahoo.com>
2021-11-23 01:05:35 +1300

    GON-002: Create Features, Hooks, Step Definition, Page Object

---
+++ MyStoreTestProject/Step Definitions/Login_steps.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace MyStoreTestProject.Step_Definitions
+{
+    [Binding]
+    public sealed class Login_steps
+    {
+
+        public Login_steps(ScenarioContext scenarioContext)
+        {
+
+        }
+
+ 
+    }
+}
