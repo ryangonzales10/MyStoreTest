f0efb9bb925877a1af8f639b69ad47297a6cd372
GONZALES-LAPTOP\rvg_2 <rvg_23@yahoo.com>
2021-11-23 13:06:37 +1300

    GON-003: Login Test

--- MyStoreTestProject/Step Definitions/Login_steps.cs
+++ MyStoreTestProject/Step Definitions/Login_steps.cs
@@ -3,18 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using TechTalk.SpecFlow;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace MyStoreTestProject.Step_Definitions
 {
     [Binding]
     public sealed class Login_steps
     {
+        ChromeDriver Browser = new ChromeDriver();
 
-        public Login_steps(ScenarioContext scenarioContext)
+        [Given(@"the User navigates to http://automationpractice\.com/")]
+        public void GivenTheUserNavigatesToHttpAutomationpractice_Com()
         {
-
+            Browser.Navigate().GoToUrl("http://automationpractice.com/");
         }
 
- 
+        [When(@"the user clicks on Sign In Button at the top right")]
+        public void WhenTheUserClicksOnSignInButtonAtTheTopRight()
+        {
+            Browser.FindElement(By.XPath("//a[@title='Log in to your customer account']")).Click();
+        }
+
+        [When(@"the User inputs the correct credentials")]
+        public void WhenTheUserInputsTheCorrectCredentials()
+        {
+            Browser.FindElement(By.Id("email")).SendKeys("archersphoto4@gmail.com");
+            Browser.FindElement(By.Id("passwd")).SendKeys("password01");
+        }
+
+        [When(@"the User clicks on the green Sign In button")]
+        public void WhenTheUserClicksOnTheGreenSignInButton()
+        {
+            Browser.FindElement(By.Id("SubmitLogin")).Click();
+        }
+
+
+        [Then(@"the User should be able to enter their accout")]
+        public void ThenTheUserShouldBeAbleToEnterTheirAccout()
+        {
+            Browser.Close();
+            Browser.Quit();
+        }
+
+
+
     }
 }
