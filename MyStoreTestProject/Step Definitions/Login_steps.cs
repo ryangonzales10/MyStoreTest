c6437953a2491b1c935d72a544afd8520efe5083
GONZALES-LAPTOP\rvg_2 <rvg_23@yahoo.com>
2021-11-24 00:19:41 +1300

    GON-006: Add tests for logout, invalid and incomplete credentials, wrong email format

--- MyStoreTestProject/Step Definitions/Login_steps.cs
+++ MyStoreTestProject/Step Definitions/Login_steps.cs
@@ -13,13 +13,20 @@ namespace MyStoreTestProject.Step_Definitions
     [Binding]
     public sealed class Login_steps
     {
+
+        #region Given
         [Given(@"the User navigates to http://automationpractice\.com/")]
         public void GivenTheUserNavigatesToHttpAutomationpractice_Com()
         {
             Browser.Webdriver.Navigate().GoToUrl("http://automationpractice.com/");
         }
 
-        [When(@"the user clicks on Sign In Button at the top right")]
+        #endregion
+
+
+        #region When
+
+        [When(@"the User clicks on Sign In Button at the top right")]
         public void WhenTheUserClicksOnSignInButtonAtTheTopRight()
         {
             Page.SignIn1();
@@ -39,13 +46,48 @@ namespace MyStoreTestProject.Step_Definitions
             Page.SignIn2();
         }
 
+        [When(@"the User clicks logout")]
+        public void WhenTheUserClicksLogout()
+        {
+            Browser.Webdriver.FindElement(By.XPath("//a[@title='Log me out']")).Click();
 
-        [Then(@"the User should be able to enter their accout")]
-        public void ThenTheUserShouldBeAbleToEnterTheirAccout()
+        }
+
+        [When(@"the User enters an incorrect User Name and Password")]
+        public void WhenTheUserEntersAnIncorrectUserNameAndPassword()
+        {
+            Page.Username("test@test.com");
+            Page.Password("hello123");
+        }
+
+        [When(@"the User inputs an email with an incorrect format")]
+        public void WhenTheUserInputsAnEmailWithAnIncorrectFormat()
+        {
+            Page.Username("blahblahblah");
+            Page.Password("hello123");
+            Page.SignIn2();
+        }
+
+        [When(@"the User leaves the password blank")]
+        public void WhenTheUserLeavesThePasswordBlank()
+        {
+            Page.Username("testblankpw@test.com");
+            Page.ClearPassword();
+            Page.SignIn2();
+        }
+
+        #endregion
+
+
+        #region Then
+
+        [Then(@"the User should be able to enter their account")]
+        public void ThenTheUserShouldBeAbleToEnterTheirAccount()
         {
             try
             {
                 Browser.Webdriver.FindElement(By.Id("my-account")); //goes to catch when element is missing
+                Browser.Webdriver.FindElement(By.XPath("//span[text()='Tester McTest']")); //checks if my username is visible
             }
             catch(Exception e)
             {
@@ -54,7 +96,67 @@ namespace MyStoreTestProject.Step_Definitions
             }
         }
 
+        [Then(@"the User is signed out and directed back to login page")]
+        public void ThenTheUserIsSignedOutAndDirectedBackToLoginPage()
+        {
+            try
+            {
+                Browser.Webdriver.FindElement(By.XPath("//a[@class='login']"));
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                throw e;
+            }
+        }
 
+        [Then(@"an authentication error should appear")]
+        public void ThenAnAuthenticationErrorShouldAppear()
+        {
+            try
+            {
+                Browser.Webdriver.FindElement(By.XPath("//div[@class='alert alert-danger']/ol/li[text()='Authentication failed.']"));
+                Page.ClearUsername();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                throw e;
+            }
+
+        }
+
+        [Then(@"an Invalid Email Address error should appear")]
+        public void ThenAnInvalidEmailAddressErrorShouldAppear()
+        {
+            try
+            {
+                Browser.Webdriver.FindElement(By.XPath("//div[@class='alert alert-danger']/ol/li[text()='Invalid email address.']"));
+                Page.ClearUsername();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                throw e;
+            }
+        }
+
+        [Then(@"a Password is Required error should appear")]
+        public void ThenAPasswordIsRequiredErrorShouldAppear()
+        {
+            try
+            {
+                Browser.Webdriver.FindElement(By.XPath("//div[@class='alert alert-danger']/ol/li[text()='Password is required.']"));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                throw e;
+            }
+        }
+
+
+        #endregion
 
     }
 }
