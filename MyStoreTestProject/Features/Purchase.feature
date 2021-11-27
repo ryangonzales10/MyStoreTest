830eedd3faf51963e8eff291bae1eea259def5e3
GONZALES-LAPTOP\rvg_2 <rvg_23@yahoo.com>
2021-11-28 00:11:57 +1300

    GON-007: Create Purchase feature and steps

---
+++ MyStoreTestProject/Features/Purchase.feature
@@ -0,0 +1,9 @@
+﻿Feature: As a User, I should be able to purchase an item
+	End-to-end test for purchasing an item
+
+Background: User goes to the My Store site 
+	Given the User navigates to http://automationpractice.com/
+
+Scenario: Check if Quick View works
+	Given the User clicks Quick View
+	Then the Item's description, price and add to cart button should appear
---
+++ MyStoreTestProject/Step Definitions/Purchase_steps.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyStoreTestProject.Page_Objects;
+
+namespace MyStoreTestProject.Step_Definitions
+{
+    [Binding]
+    public sealed class Purchase_steps
+    {
+
+        #region Given
+        [Given(@"the User clicks Quick View")]
+        public void GivenTheUserClicksQuickView()
+        {
+
+        }
+
+        #endregion
+
+
+        #region When
+
+
+
+        #endregion
+
+
+        #region Then
+        [Then(@"the Item's description, price and add to cart button should appear")]
+        public void ThenTheItemSDescriptionPriceAndAddToCartButtonShouldAppear()
+        {
+
+        }
+
+
+        #endregion
+
+    }
+}
