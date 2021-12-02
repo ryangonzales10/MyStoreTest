bbbc9b42a3b2327731786b26d0d70569a647de62
GONZALES-LAPTOP\rvg_2 <rvg_23@yahoo.com>
2021-12-02 21:51:02 +1300

    SEL-002: Add Configuration Manager to Unit Test. App.Config.

--- SeleniumWebdriver/UnitTest1.cs
+++ SeleniumWebdriver/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Configuration;
 
 namespace SeleniumWebdriver
 {
@@ -9,6 +10,13 @@ namespace SeleniumWebdriver
         [TestMethod]
         public void TestMethod1()
         {
+            //ConfigurationManager is from a DLL we added called System.Configuration
+            //It enables me to read a configuration file "App.config" 
+            //You need to have <configuration> and <appSettings> in App.config
+            Console.WriteLine(ConfigurationManager.AppSettings.Get("Browser"));
+            Console.WriteLine(ConfigurationManager.AppSettings.Get("Username"));
+            Console.WriteLine(ConfigurationManager.AppSettings.Get("Password"));
+
         }
     }
 }
