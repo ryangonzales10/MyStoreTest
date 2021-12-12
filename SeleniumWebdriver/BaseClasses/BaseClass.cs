80869673bc4a8f24053f6e3a3e68b327b4c1212b
GONZALES-LAPTOP\rvg_2 <rvg_23@yahoo.com>
2021-12-12 22:59:17 +1300

    SEL-004: Create Base Class

---
+++ SeleniumWebdriver/BaseClasses/BaseClass.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using SeleniumWebdriver.Settings;
+using SeleniumWebdriver.Configuration;
+using SeleniumWebdriver.CustomException;
+using OpenQA.Selenium.Firefox;
+
+//This takes care of launching the browsers
+namespace SeleniumWebdriver.BaseClasses
+{
+    [TestClass]
+    public class BaseClass
+    {
+        private static IWebDriver GetChromeDriver()
+        {
+            IWebDriver driver = new ChromeDriver();
+            return driver;
+        }
+
+        private static IWebDriver GetFirefoxDriver()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            return driver;
+        }
+
+        [AssemblyInitialize]
+        public static void InitWebdriver(TestContext tc) //makes this method run first 
+        {
+            ObjectRepository.Config = new AppConfigReader();
+            switch (ObjectRepository.Config.GetBrowser())
+            {
+                case BrowserType.Chrome:
+                    ObjectRepository.Driver = GetChromeDriver();
+                    break;
+
+                case BrowserType.Firefox:
+                    ObjectRepository.Driver = GetFirefoxDriver();
+                    break;
+                default:
+                    throw new NoSuitableDriverFound($"Driver Not Found {ObjectRepository.Config.GetBrowser().ToString()}");
+            }
+        }
+    }
+}
