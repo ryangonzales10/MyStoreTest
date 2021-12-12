e9af4b41a46bf2f6b8d3b5e727ebbc7ecb866ca4
GONZALES-LAPTOP\rvg_2 <rvg_23@yahoo.com>
2021-12-12 23:02:17 +1300

    SEL-004: Create my own exception

---
+++ SeleniumWebdriver/CustomException/NoSuitableDriverFound.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//this is how you create a custom exception
+namespace SeleniumWebdriver.CustomException
+{
+    class NoSuitableDriverFound : Exception //inherit the exception class
+    {
+        public NoSuitableDriverFound(string msg) : base(msg) //msg is the argument
+        {
+
+        }
+    }
+}
