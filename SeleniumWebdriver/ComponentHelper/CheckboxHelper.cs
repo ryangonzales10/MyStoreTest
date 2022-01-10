3c5988b45ff67900dd74dd5b4e985e6a3e9530db
GONZALES-LAPTOP\rvg_2 <rvg_23@yahoo.com>
2022-01-11 02:31:07 +1300

    SEL-007: Verifying if Checkbox is checked or not using GetAttribute

--- SeleniumWebdriver/ComponentHelper/CheckboxHelper.cs
+++ SeleniumWebdriver/ComponentHelper/CheckboxHelper.cs
@@ -1,4 +1,5 @@
 ﻿using OpenQA.Selenium;
+using SeleniumWebdriver.Settings;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,10 +12,29 @@ namespace SeleniumWebdriver.ComponentHelper
     {
         //this is another approach
         private static IWebElement element;
-        public static void CheckCheckbox(By Locator)
+        public static void CheckedCheckbox(By Locator)
         {
             element = GenericHelper.GetElement(Locator);
             element.Click();
         }
+
+
+        public static bool IsCheckboxChecked(By Locator) //study me
+        {
+            
+            element = GenericHelper.GetElement(Locator);
+
+            //ObjectRepository.Driver.FindElement(Locator).GetAttribute("checked");
+            string flag = element.GetAttribute("class");
+            //get the attribute checked and store it in string flag
+
+            if (flag == null) return false;
+            else return flag.Equals("true") || flag.Equals("checked");
+            //true if the value parameter is the same as the value of this instance, otherwise false. If value is null, returns false
+
+        }
+
+
+
     }
 }
--- SeleniumWebdriver/TestScript/Checkbox/TestCheckBox.cs
+++ SeleniumWebdriver/TestScript/Checkbox/TestCheckBox.cs
@@ -1,5 +1,7 @@
-﻿using OpenQA.Selenium;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
 using SeleniumWebdriver.ComponentHelper;
+using SeleniumWebdriver.Settings;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,12 +10,17 @@ using System.Threading.Tasks;
 
 namespace SeleniumWebdriver.TestScript.Checkbox
 {
+    [TestClass]
     public class TestCheckBox
     {
-
-        public static void TestBox()
+        [TestMethod]
+        public void TestBox()
         {
-
+            NavigationHelper.NavigateToUrl(ObjectRepository.Config.GetWebsite());
+            ObjectRepository.Driver.FindElement(By.XPath("//a[@title='Women']")).Click(); //clicks Women section
+            CheckboxHelper.CheckedCheckbox(By.Id("uniform-layered_category_4"));
+            CheckboxHelper.IsCheckboxChecked(By.XPath("//div[@id='uniform-layered_category_4']/span")); //this is showing false!!! 
+                 
         }
     }
 }
