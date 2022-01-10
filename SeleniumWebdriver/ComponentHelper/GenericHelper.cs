2a428229088cce79f5a9bb3b256f3ca1c9f4bf9b
GONZALES-LAPTOP\rvg_2 <rvg_23@yahoo.com>
2022-01-10 15:24:07 +1300

    SEL-007: Create IsElementPresent check, and add tests for TextBox

---
+++ SeleniumWebdriver/ComponentHelper/GenericHelper.cs
@@ -0,0 +1,42 @@
+﻿using OpenQA.Selenium;
+using SeleniumWebdriver.Settings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumWebdriver.ComponentHelper
+{
+    public class GenericHelper
+    {
+        public static bool IsElementPresent (By Locator)
+        {
+            //method should hava e locator that's unique! (not use why not use FindELement instead???)
+            try
+            {
+                return ObjectRepository.Driver.FindElements(Locator).Count == 1; //it will return me the size and compare it to 1
+                //check if element id present or not. If it is there, it will return me a count as 1, and this entire statement will return true
+
+                //if it's not there, it will return me a zero, and this is a false condition 
+                //in any other cases, it will go to the catch exception
+               
+                //Findelement - finds a single element, returns single webelement
+                //FindELements - finds multiple elements, returns a list of webelements
+            }
+            catch(Exception)
+            {
+                return false;
+            }
+
+        }
+
+        public static IWebElement GetElement(By Locator)
+        {
+            if (IsElementPresent(Locator))
+                return ObjectRepository.Driver.FindElement(Locator);
+            else
+                throw new NoSuchElementException($"Element Not Found: {Locator.ToString()}");
+        }
+    }
+}
---
+++ SeleniumWebdriver/TestScript/TextBox/TestTextBox.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using SeleniumWebdriver.ComponentHelper;
+using SeleniumWebdriver.Settings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SeleniumWebdriver.TestScript.TextBox
+{
+    [TestClass]
+    public class TestTextBox
+    {
+        [TestMethod]
+        public void TextBox()
+        {
+            NavigationHelper.NavigateToUrl(ObjectRepository.Config.GetWebsite());
+            ObjectRepository.Driver.FindElement(By.XPath("//a[@class='login']")).Click();
+            ObjectRepository.Driver.FindElement(By.Id("email")).SendKeys("rvg_23@yahoo.com");
+            ObjectRepository.Driver.FindElement(By.Id("passwd")).SendKeys("burubugudstoystugudunstuy");
+            ObjectRepository.Driver.FindElement(By.Id("passwd")).Clear();
+        }
+    }
+}
