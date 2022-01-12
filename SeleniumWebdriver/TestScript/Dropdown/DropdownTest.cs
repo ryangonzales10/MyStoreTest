d398329e3ef0d8632155f12de849d042419e1fa5
GONZALES-LAPTOP\rvg_2 <rvg_23@yahoo.com>
2022-01-13 10:16:46 +1300

    SEL-009: Add Dropdown Tests, initiated SelectElement to help with dropdown list

---
+++ SeleniumWebdriver/TestScript/Dropdown/DropdownTest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumWebdriver.ComponentHelper;
+using SeleniumWebdriver.Settings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumWebdriver.TestScript.Dropdown
+{
+    [TestClass]
+    public class DropdownTest
+    {
+        [TestMethod]
+        public void TestList()
+        {
+            NavigationHelper.NavigateToUrl(ObjectRepository.Config.GetWebsite());
+            ObjectRepository.Driver.FindElement(By.LinkText("Women")).Click();
+            SelectElement dropdown = new SelectElement(ObjectRepository.Driver.FindElement(By.Id("selectProductSort")));
+            dropdown.SelectByIndex(1); //selects second item on the list
+            dropdown.SelectByText("Product Name: A to Z"); //this is the visible text
+            dropdown.SelectByValue("quantity:desc"); //this is the value tag
+
+        }
+    }
+}
