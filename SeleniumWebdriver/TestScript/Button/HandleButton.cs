f56d8993f728b855b7b4dd360d406f29af726a57
GONZALES-LAPTOP\rvg_2 <rvg_23@yahoo.com>
2022-01-12 23:25:29 +1300

    SEL-008: Created ButtonHelper, ClickButton(), IsButtonEnabled() and GetButtonText() - lesson, use innterText

---
+++ SeleniumWebdriver/TestScript/Button/HandleButton.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using SeleniumWebdriver.ComponentHelper;
+using SeleniumWebdriver.Settings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumWebdriver.TestScript.Button
+{
+    [TestClass]
+    public class HandleButton
+    {
+        [TestMethod]
+        public void TestButton()
+        {
+            NavigationHelper.NavigateToUrl(ObjectRepository.Config.GetWebsite());
+            ObjectRepository.Driver.FindElement(By.XPath("//a[@class='login']")).Click();
+            TextboxHelper.TypeInTextBox(By.Id("email"), ObjectRepository.Config.GetUsername());
+            TextboxHelper.TypeInTextBox(By.Id("passwd"), ObjectRepository.Config.GetPassword());
+
+            Console.WriteLine($"Is the button enabled? {ButtonHelper.IsButtonEnabled(By.Id("SubmitLogin"))}");
+            Console.WriteLine($"The text inside the button is: {ButtonHelper.GetButtonText(By.XPath("//button[@id='SubmitLogin']/span"))}");
+
+            ButtonHelper.ClickButton(By.Id("SubmitLogin"));
+
+        }
+    }
+}
