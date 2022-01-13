178ecf202f6b6ffec5f2520ebf46c3d2d7215859
GONZALES-LAPTOP\rvg_2 <rvg_23@yahoo.com>
2022-01-13 14:06:02 +1300

    SEL-009: Create DropdownHelper class

--- SeleniumWebdriver/TestScript/Dropdown/DropdownTest.cs
+++ SeleniumWebdriver/TestScript/Dropdown/DropdownTest.cs
@@ -19,10 +19,21 @@ namespace SeleniumWebdriver.TestScript.Dropdown
         {
             NavigationHelper.NavigateToUrl(ObjectRepository.Config.GetWebsite());
             ObjectRepository.Driver.FindElement(By.LinkText("Women")).Click();
+
             SelectElement dropdown = new SelectElement(ObjectRepository.Driver.FindElement(By.Id("selectProductSort")));
-            dropdown.SelectByIndex(1); //selects second item on the list
-            dropdown.SelectByText("Product Name: A to Z"); //this is the visible text
-            dropdown.SelectByValue("quantity:desc"); //this is the value tag
+
+            dropdown.SelectByIndex(1); //selects second item on the list. or...
+            DropdownHelper.DropdownSelectByIndex(By.Id("selectProductSort"), 1);
+
+            dropdown.SelectByText("Product Name: A to Z"); //this is the visible text. or...
+            DropdownHelper.DropdownSelectByText(By.Id("selectProductSort"), "Product Name: A to Z");
+
+            dropdown.SelectByValue("quantity:desc"); //this is the value tag. or...
+            DropdownHelper.DropdownSelectByValue(By.Id("selectProductSort"), "quantity:desc");
+
+ 
+            
+
 
             //to check the selected value:
             Console.WriteLine($"The selected option of the dropdown is {dropdown.SelectedOption.Text}"); //.Text at the end converts it  to a string
@@ -41,6 +52,12 @@ namespace SeleniumWebdriver.TestScript.Dropdown
             foreach (IWebElement x in allOptionsInList)
             {
                 Console.WriteLine($"Option value: {x.GetAttribute("value")}, text: {x.Text}");
+            }
+
+            //this is using the helper class
+            foreach (string y in DropdownHelper.GetAllItems(By.Id("selectProductSort")))
+            {
+                Console.WriteLine($"Text: {y}");
             }
         }
     }
