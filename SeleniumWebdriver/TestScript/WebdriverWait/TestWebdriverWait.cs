a6ef9c0808f7957242bced2d8355a1a3e7d372bb
GONZALES-LAPTOP\rvg_2 <rvg_23@yahoo.com>
2022-01-14 14:33:51 +1300

    SEL-014: Page Load Timeout

---
+++ SeleniumWebdriver/TestScript/WebdriverWait/TestWebdriverWait.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using SeleniumWebdriver.Settings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumWebdriver.TestScript.WebdriverWait
+{
+    [TestClass]
+    public class TestWebdriverWait
+    {
+        [TestMethod]
+        public void TestWait()
+        {
+            Browser.Driver.Manage().Timeouts().PageLoad = TimeSpan.FromMilliseconds(10);
+            Browser.Driver.Navigate().GoToUrl("http://www.facebook.com");
+            Browser.Driver.FindElement(By.LinkText("Forgotten password?")).Click();
+        }
+    }
+}
